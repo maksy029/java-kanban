c501fba4f09a56934c9e6bf11e674e8411532e08
maksy029 <maksy029@yandex.ru>
2023-09-18 15:51:59 +0300

    коммит 1.1 по ТЗ6:  исправил FileBackedTasksManagerTest (запись истории просмотра задач в CSV файл

--- src/ru/practicum/task_tracker/tester/FileBackedTasksManagerTest.java
+++ src/ru/practicum/task_tracker/tester/FileBackedTasksManagerTest.java
@@ -61,6 +61,7 @@ public class FileBackedTasksManagerTest {
             System.out.println(history);
         }
 
+        
         //Печать восстановленных из СSV файла задач:
         System.out.println();
         System.out.println("Печать восстановленных из СSV файла задач");
